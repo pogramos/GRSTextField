97babdec36d0681d6b01bddf993b1da9b1f0f274
Guilherme Ramos <guilherme.hor@gmail.com>
2018-08-30 17:55:48 -0300

    Initial Commit

---
+++ GRSTextField/GRSTextField.h
@@ -0,0 +1,19 @@
+//
+//  GRSTextField.h
+//  GRSTextField
+//
+//  Created by Guilherme on 8/30/18.
+//  Copyright © 2018 Progeekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GRSTextField.
+FOUNDATION_EXPORT double GRSTextFieldVersionNumber;
+
+//! Project version string for GRSTextField.
+FOUNDATION_EXPORT const unsigned char GRSTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GRSTextField/PublicHeader.h>
+
+
